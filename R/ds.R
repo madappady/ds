ea2d54b0fd9d31799f18a26a70087f93e8b7e07b
Dr Sajjan Madappady <dynamicsajjan@gmail.com>
2025-04-11 01:23:15 +0530

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,17 @@
+#' Create histogram, boxplot and numeric summary
+#' @export
+#' @param x numeric variable description
+ds<- function(x){
+  # 1 row and 2 colmns
+  par(mfrow = c(1,2))
+  #histogram
+  hist(x, col = rainbow(30))
+  # Box plot
+  boxplot(x, col = 'green')
+  par(mfrow = c(1, 1))
+  #Numeric Summery
+  data.frame(min = min(x),
+             median = median(x),
+             mean = mean(x),
+             max = max(x))
+}
